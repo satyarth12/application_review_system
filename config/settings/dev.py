93451777344c748f954710b346d8dcf878bd60f5
Satyarth <srk.satyarth.1@gmail.com>
2022-10-02 20:57:21 +0530

    isolated settings for development and production

---
+++ config/settings/dev.py
@@ -0,0 +1,29 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Cors Settings
+# If True, all origins will be allowed
+CORS_ALLOW_ALL_ORIGINS = ENV_INFO_.getboolean("CORS_ALLOW_ALL")
+
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header'
+        }
+    }
+}
